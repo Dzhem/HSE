cbc3395aef2df1e149dd03d3998dc8db411859dc
Zemchuzhnov Dmitriy <zhemchuzhnov.d@gmail.com>
2020-01-20 10:57:50 +0400

    Оптимизировал условия задачи Координатные четверти

--- 2nd_week_coordinate_quarters.py
+++ 2nd_week_coordinate_quarters.py
@@ -6,23 +6,27 @@ y2 = int(input())
 quart_x = 0
 quart_y = 0
 
-if x1 > 0 and x2 > 0:
-    quart_x = 1
-elif x1 > 0 and x2 < 0:
-    quart_x = 2
-elif x1 < 0 and x2 < 0:
-    quart_x = 3
-else:
-    quart_x = 4
+if x1 > 0:
+    if x2 > 0:
+        quart_x = 1
+    else:
+        quart_x = 2
+elif x1 < 0:
+    if x2 < 0:
+        quart_x = 3
+    else:
+        quart_x = 4
 
-if y1 > 0 and y2 > 0:
-    quart_y = 1
-elif y1 > 0 and y2 < 0:
-    quart_y = 2
-elif y1 < 0 and y2 < 0:
-    quart_y = 3
-else:
-    quart_y = 4
+if y1 > 0:
+    if y2 > 0:
+        quart_y = 1
+    else:
+        quart_y = 2
+elif y1 < 0:
+    if y2 < 0:
+        quart_y = 3
+    else:
+        quart_y = 4
 
 if quart_x == quart_y:
     print('YES')
