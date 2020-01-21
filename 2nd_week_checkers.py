0d0e8456c1f4de6f03945239a8973c2e255b8e35
Zhemchuzhnov Dmitry <zhemchuzhnov.d@gmail.com>
2020-01-21 21:36:26 +0400

    Окончательное решение задачи про шашки (добавил проверку цвета поля)

--- 2nd_week_checkers.py
+++ 2nd_week_checkers.py
@@ -4,6 +4,9 @@ x2 = int(input())
 y2 = int(input())
 
 if y2 > y1 and (x1 - (y2 - y1) <= x2 <= x1 + (y2 - y1)):
-    print('YES')
+    if (y2 % 2 == 0 and x2 % 2 == 0) or (y2 % 2 != 0 and x2 % 2 != 0):
+        print('YES')
+    else:
+        print('NO')
 else:
     print('NO')
