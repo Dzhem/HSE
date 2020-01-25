1a519cfaedd92d6a66f264a38e8855f86e92f310
Zhemchuzhnov Dmitry <zhemchuzhnov.d@gmail.com>
2020-01-25 15:43:21 +0400

    Окончательный вариант решение задачи Коробки

--- 2nd_week_boxes.py
+++ 2nd_week_boxes.py
@@ -27,21 +27,3 @@ elif a1 >= a2 and b1 >= b2 and c1 >= c2:
     print('The first box is larger than the second one')
 else:
     print('Boxes are incomparable')
-
-# if ((a1 == a2 and b1 == b2 and c1 == c2) or
-#         (a1 == b2 and b1 == a2 and c1 == c2) or
-#         (a1 == a2 and c1 == b2 and b1 == c2) or
-#         (a1 == c2 and b1 == b2 and c1 == a2)):
-#     print('Boxes are equal')
-# elif ((a1 <= a2 and b1 <= b2 and c1 <= c2) or
-#         (a1 <= b2 and b1 <= a2 and c1 <= c2) or
-#         (a1 <= a2 and c1 <= b2 and b1 <= c2) or
-#         (a1 <= c2 and b1 <= b2 and c1 <= a2)):
-#     print('The first box is smaller than the second one')
-# elif ((a1 >= a2 and b1 >= b2 and c1 >= c2) or
-#         (a1 >= b2 and b1 >= a2 and c1 >= c2) or
-#         (a1 >= a2 and c1 >= b2 and b1 >= c2) or
-#         (a1 >= c2 and b1 >= b2 and c1 >= a2)):
-#     print('The first box is larger than the second one')
-# else:
-#     print('Boxes are incomparable')
