0ff81be5a1ca1f9dc2e9fec0fc9ef734fd6c7269
Zhemchuzhnov Dmitry <zhemchuzhnov.d@gmail.com>
2020-01-25 14:37:50 +0400

    Решение задачи Коробки проходит не все тесты

--- 2nd_week_boxes.py
+++ 2nd_week_boxes.py
@@ -15,5 +15,10 @@ elif ((a1 <= a2 and b1 <= b2 and c1 <= c2) or
         (a1 <= a2 and c1 <= b2 and b1 <= c2) or
         (a1 <= c2 and b1 <= b2 and c1 <= a2)):
     print('The first box is smaller than the second one')
-else:
+elif ((a1 >= a2 and b1 >= b2 and c1 >= c2) or
+        (a1 >= b2 and b1 >= a2 and c1 >= c2) or
+        (a1 >= a2 and c1 >= b2 and b1 >= c2) or
+        (a1 >= c2 and b1 >= b2 and c1 >= a2)):
     print('The first box is larger than the second one')
+else:
+    print('Boxes are incomparable')
