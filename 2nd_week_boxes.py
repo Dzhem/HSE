5bc88328f6f8eea204d1118e8895d27e80ebe9ec
Zhemchuzhnov Dmitry <zhemchuzhnov.d@gmail.com>
2020-01-25 15:41:40 +0400

    Вариант решение задачи Коробки с ориентацией длинн сторон коробок по убыванию

--- 2nd_week_boxes.py
+++ 2nd_week_boxes.py
@@ -5,20 +5,43 @@ a2 = int(input())
 b2 = int(input())
 c2 = int(input())
 
-if ((a1 == a2 and b1 == b2 and c1 == c2) or
-        (a1 == b2 and b1 == a2 and c1 == c2) or
-        (a1 == a2 and c1 == b2 and b1 == c2) or
-        (a1 == c2 and b1 == b2 and c1 == a2)):
+if a1 < b1:
+    (a1, b1) = (b1, a1)
+if b1 < c1:
+    (b1, c1) = (c1, b1)
+if a1 < b1:
+    (a1, b1) = (b1, a1)
+
+if a2 < b2:
+    (a2, b2) = (b2, a2)
+if b2 < c2:
+    (b2, c2) = (c2, b2)
+if a2 < b2:
+    (a2, b2) = (b2, a2)
+
+if a1 == a2 and b1 == b2 and c1 == c2:
     print('Boxes are equal')
-elif ((a1 <= a2 and b1 <= b2 and c1 <= c2) or
-        (a1 <= b2 and b1 <= a2 and c1 <= c2) or
-        (a1 <= a2 and c1 <= b2 and b1 <= c2) or
-        (a1 <= c2 and b1 <= b2 and c1 <= a2)):
+elif a1 <= a2 and b1 <= b2 and c1 <= c2:
     print('The first box is smaller than the second one')
-elif ((a1 >= a2 and b1 >= b2 and c1 >= c2) or
-        (a1 >= b2 and b1 >= a2 and c1 >= c2) or
-        (a1 >= a2 and c1 >= b2 and b1 >= c2) or
-        (a1 >= c2 and b1 >= b2 and c1 >= a2)):
+elif a1 >= a2 and b1 >= b2 and c1 >= c2:
     print('The first box is larger than the second one')
 else:
     print('Boxes are incomparable')
+
+# if ((a1 == a2 and b1 == b2 and c1 == c2) or
+#         (a1 == b2 and b1 == a2 and c1 == c2) or
+#         (a1 == a2 and c1 == b2 and b1 == c2) or
+#         (a1 == c2 and b1 == b2 and c1 == a2)):
+#     print('Boxes are equal')
+# elif ((a1 <= a2 and b1 <= b2 and c1 <= c2) or
+#         (a1 <= b2 and b1 <= a2 and c1 <= c2) or
+#         (a1 <= a2 and c1 <= b2 and b1 <= c2) or
+#         (a1 <= c2 and b1 <= b2 and c1 <= a2)):
+#     print('The first box is smaller than the second one')
+# elif ((a1 >= a2 and b1 >= b2 and c1 >= c2) or
+#         (a1 >= b2 and b1 >= a2 and c1 >= c2) or
+#         (a1 >= a2 and c1 >= b2 and b1 >= c2) or
+#         (a1 >= c2 and b1 >= b2 and c1 >= a2)):
+#     print('The first box is larger than the second one')
+# else:
+#     print('Boxes are incomparable')
