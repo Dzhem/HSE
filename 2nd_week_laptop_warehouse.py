995c92ac015e3ad326049b449540c7c6f59aa512
Zhemchuzhnov Dmitry <zhemchuzhnov.d@gmail.com>
2020-01-25 23:01:35 +0400

    Решение задачи Складирование ноутбуков (40/100)

--- 2nd_week_laptop_warehouse.py
+++ 2nd_week_laptop_warehouse.py
@@ -5,22 +5,18 @@ a2 = int(input())
 b2 = int(input())
 c2 = int(input())
 
-v1 = a1 * b1 * c1
-v2 = a2 * b2 * c2
+if a1 < b1:
+    (a1, b1) = (b1, a1)
+if b1 < c1:
+    (b1, c1) = (c1, b1)
+if a1 < b1:
+    (a1, b1) = (b1, a1)
 
-min_side1 = a1
-if min_side1 > b1:
-    min_side1 = b1
-if min_side1 > c1:
-    min_side1 = c1
+if a2 < b2:
+    (a2, b2) = (b2, a2)
+if b2 < c2:
+    (b2, c2) = (c2, b2)
+if a2 < b2:
+    (a2, b2) = (b2, a2)
 
-min_side2 = a2
-if min_side2 > b2:
-    min_side2 = b2
-if min_side2 > c2:
-    min_side2 = c2
-
-if min_side2 > min_side1:
-    print(0)
-else:
-    print(v1 // v2)
+print((a1 // a2) * (b1 // b2) * (c1 // c2))
